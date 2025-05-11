f6a39ce1cb7f26e539ad314cfbbd674ffe896f76
mkrtchyantigran <tigran.mkrtchyan1@outlook.com>
2025-05-11 14:10:05 +0200

    Created React with Vite and added TailwindCSS

---
+++ frontend/src/components/app/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+
+  return (
+   <div className="flex flex-col items-center justify-center text-center bg-gray-600 dark:bg-[#112d49]">
+       <header className="bg-gray-600 dark:bg-[#0a1a2d] text-white w-full mb4">
+        <h1 className="text-4x1 my-8">Book Library App</h1>
+    </header>
+   </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+
+
+export default Header = () => {
+  return (
+    <header className="bg-gray-600 dark:bg-[#0a1a2d] text-white w-full mb4">
+        <h1 className="text-4x1 my-8">Book Library App</h1>
+    </header>
+  )
+}
