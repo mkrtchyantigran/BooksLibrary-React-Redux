f763da00bddd5ffec0ea4e854368297a883b3ba1
mkrtchyantigran <tigran.mkrtchyan1@outlook.com>
2025-06-08 16:49:03 +0200

    fixed title responsive issue

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -3,7 +3,9 @@
 const Header = () => {
   return (
     <header className=" bg-[#0f171d] dark:bg-black text-white w-full mb-4">
-        <h1 className="text-4x1 my-8 text-center">Book Library App</h1>
+        <h1 className="text-4x1 max-md:text-3xl my-8 text-center">
+          Book Library App
+        </h1>
     </header>
   )
 }
